5c9bb9507de27be73f1e3ac1813c1255156b6388
Sergei Illarionov <butteff.ru@gmail.com>
2024-04-14 20:39:13 +0300

    refactoring to update settings

--- modules/database.rb
+++ modules/database.rb
@@ -3,7 +3,7 @@ require "sqlite3"
 
 module Database
     class Sqlt
-        #include Singleton 
+        #include Singleton
 
         def initialize
             @db = db_init
@@ -11,13 +11,9 @@ module Database
             query('create table if not exists settings (url varchar(64), is_shot boolean, interval int);')
             setup = read('select * from settings')
             if setup.empty?
-                settings_default = {url: "https://track.butteff.ru", is_shot: 1, interval: 5}
-                query("INSERT INTO settings (url, is_shot, interval) VALUES (?, ?, ?)", [settings_default[:url], settings_default[:is_shot], settings_default[:interval]] )
+                settings = {url: "https://track.butteff.ru", is_shot: 1, interval: 55}        
+                write('settings', settings)
             end
-        end
-
-        def query(sql, data = nil)
-            @db.execute(sql, data)
         end
 
         def read(sql)
@@ -28,8 +24,32 @@ module Database
             res
         end
 
+        def write(tbl, hash_data, cleanup=false)
+            questions = '?'
+            looop = hash_data.length-1
+            looop.times{questions.prepend('?, ')}
+            values = hash_data.keys.map{|a| a.to_s}.to_s[1..-2].sub(':', '').gsub('"', '')
+
+            qry = "INSERT INTO"
+            qry+= ' '+tbl
+            qry+= ' ('+values+') '
+            qry+= 'VALUES ('+questions+')'
+
+            array = []
+            keys = hash_data.keys
+            keys.each do |key|
+                array += [hash_data[key]]
+            end
+            query('DELETE FROM '+tbl+' WHERE 1') if cleanup
+            query(qry, array)
+        end
+
         private
 
+        def query(sql, data = nil)
+            @db.execute(sql, data)
+        end
+        
         def db_init
             SQLite3::Database.new "track.db"
         end
--- modules/iostream.rb
+++ modules/iostream.rb
@@ -5,18 +5,45 @@ include Database
 module Iostream    
     @@sqlt = Sqlt.new
     @@COLOR_HEADER = 'sienna'
+    @@COLOR_KEY = 'red'
     @@SPACERS = 30
-
-    def io_print(info)
-        sql = 'select * from '+info
+    @@KEY_SPACERS = 20
+    
+    def io_set_one(tbl, key, value)
+        sql = 'select * from '+tbl
         res = @@sqlt.read(sql)
-        send('print_'+info, res[0])
+        hash_data= res[0]
+        hash_data[key] = value
+        @@sqlt.write(tbl, hash_data, true)
+    end
+
+    def io_get(tbl, and_print=true)
+        sql = 'select * from '+tbl
+        res = @@sqlt.read(sql)
+        send('print_'+tbl, res) if and_print
     end
 
     def print_settings(res)
-        text = Rainbow("Logs Push URL: ").send(@@COLOR_HEADER).ljust(@@SPACERS)+res['url'].to_s
-        text += "\n"+Rainbow("Screenshots: ").send(@@COLOR_HEADER).ljust(@@SPACERS) + enabled = res['is_shot'] == 1 ? 'enabled' : 'disabled'
-        text += "\n"+Rainbow("Interval: ").send(@@COLOR_HEADER).ljust(@@SPACERS)+res['interval'].to_s+' mins between screenshots' if res['is_shot'] == 1
+        res = res.first
+        
+        text = Rainbow("Logs Push URL: ").send(@@COLOR_HEADER).ljust(@@SPACERS)
+        text += Rainbow("[url] ").send(@@COLOR_KEY).ljust(@@KEY_SPACERS)
+        text += res['url'].to_s
+        text += "\n"
+
+        text += Rainbow("Screenshots: ").send(@@COLOR_HEADER).ljust(@@SPACERS)
+        text += Rainbow("[is_shot] ").send(@@COLOR_KEY).ljust(@@KEY_SPACERS)
+        text += enabled = res['is_shot'] == 1 ? 'enabled' : 'disabled'
+        text += "\n"
+
+        text += Rainbow("Interval: ").send(@@COLOR_HEADER).ljust(@@SPACERS)
+        text += Rainbow("[interval] ").send(@@COLOR_KEY).ljust(@@KEY_SPACERS) if res['is_shot'] == 1
+        text += res['interval'].to_s+' mins between screenshots' if res['is_shot'] == 1
+        
+        text += "\n\n"
+        text += "Use \"track settings {key} {value}\" to change these params, for example:" 
+        text += "\n\"track settings is_shot 0\" or \"track settings interval 10\""
+
         puts text
     end
 end    
